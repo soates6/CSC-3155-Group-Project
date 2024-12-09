4afa12426d02ca2b62be3373b95e33e5b4f0f5f7
Jonathan Park <jjpark1@my.waketech.edu>
2024-12-08 23:13:42 -0500

    fixed pages so csrf token is included

--- SafeApply/settings.py
+++ SafeApply/settings.py
@@ -116,3 +116,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
+]
